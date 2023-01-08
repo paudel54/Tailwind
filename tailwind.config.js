29e8547e8f1ddbec042a1f1f740fa48e9fb6cb4e
paudel54 <2sansrit@gmail.com>
2023-01-08 22:18:31 +0545

    import fonts creating bug it got fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
+
   content: [
-    './public/**/*.{html,js}',
+    './public/*.{html,js}',
     './public/index.html',
+    './public/output.css',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bookmark-purple': '#5267Df',
+        'bookmark-red': '#FA5959',
+        'bookmark-blue': '#0C0FD4 ',
+        'bookmark-grey': '#9194A2',
+        'bookmark-white': '#f7f7f7',
+      },
+    },
+
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }
+    },
+
   },
   plugins: [],
 }
