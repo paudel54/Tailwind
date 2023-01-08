11d86a88bc4f50a07d7ce59f3dd7d9ef25cdc779
paudel54 <2sansrit@gmail.com>
2023-01-08 14:09:42 +0545

    adding container to center by defult onto theme under tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  purge: [],
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {},
+    //By default making container to be center 
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 }
+
+
