b98a2cabbfb4aeadb9072a24e631f594850465f0
paudel54 <2sansrit@gmail.com>
2023-01-08 19:08:34 +0545

    Complete setup is ready for tailwind new version updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './public/**/*.{html,js}',
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
