b3e7cb67133f671921991ea2025d28e9f72374f0
paudel54 <2sansrit@gmail.com>
2023-01-08 17:17:16 +0545

    added google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,20 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {},
+    //Defining custom font family
+    fontFamily: {
+      Poppins: ['Poppins, sans-serif'],
+    },
+
     //By default making container to be center 
     container: {
       center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
     },
   },
   plugins: [],
