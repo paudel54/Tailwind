dacbe3311c456cf93d98d1f82109002cfebd9820
paudel54 <2sansrit@gmail.com>
2023-01-08 18:51:18 +0545

    final setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/**/*.{html,js}',
-  ],
+  content: [],
   theme: {
     extend: {},
-    container: {
-      center: true,
-      padding: '1rem',
-      screens: {
-        lg: '1124px',
-        xl: '1124px',
-        '2xl': '1124px',
-      },
-    },
-
   },
   plugins: [],
 }
