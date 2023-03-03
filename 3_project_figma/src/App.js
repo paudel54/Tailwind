d4f1298adf91c749349897991960a817801b2633
paudel54 <2sansrit@gmail.com>
2023-03-03 15:30:54 +0545

    tailwind setup in react done

---
+++ 3_project_figma/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="bg-green-300 ">
+      Hello I am react learning tailwind;
+    </div>
+  );
+}
+
+export default App;
+
+// npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
